3b474f0faf14a10ad35fa5b35a8dfaced818dd65
raffael-dwyyana <raffael.dwyyana@univ-tlse3.fr>
2023-11-12 13:57:06 +0100

    exo 2.1 finished

---
+++ src/personnages/Humain.java
@@ -0,0 +1,52 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonFavorite;
+	private int argent;
+	
+	public Humain(String nom, String boissonFavourite, int argent) {
+		this.nom = nom;
+		this.boissonFavorite = boissonFavourite;
+		this.argent = argent;
+	}
+	
+	public void parler(String texte) {
+		System.out.println("(" + nom + ") - " + texte);
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public int getArgent() {
+		return argent;
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle " + nom + " et j'aime boire du " + boissonFavorite);
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boissonFavorite + " ! GLOUPS !");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if(argent > prix) {
+			parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous.");
+			perdreArgent(prix);
+		}
+		
+		else {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offir " + bien + " à " + prix + " sous.");
+		}
+	}
+	
+	public void gagnerArgent(int gain) {
+		argent += gain;
+	}
+	
+	public void perdreArgent(int perte) {
+		argent -= perte;
+	}
+}
